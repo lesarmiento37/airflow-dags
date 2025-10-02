600854a1829ce4abf39c24078b6c079e2d07af0a
Leonardo Sarmiento <lesarmiento@unal.edu.co>
2025-10-02 12:30:06 -0500

    Adding dags

---
+++ dags/leonardo_dag.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+# Definición básica del DAG
+with DAG(
+    dag_id="hello_leonardo",
+    start_date=datetime(2025, 1, 1),
+    schedule_interval="@daily",   # se ejecuta diario
+    catchup=False,
+    tags=["example"]
+) as dag:
+
+    # Tarea con BashOperator
+    say_hello = BashOperator(
+        task_id="say_hello",
+        bash_command='echo "Hola Leonardo"'
+    )
+
+    say_hello
